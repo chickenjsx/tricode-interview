9d65488452f5609c94142a46bc8700508c42bf85
chickenjsx <augustineosezua1@gmail.com>
2025-08-01 16:27:15 -0400

    updates from work

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import models, { sequelize } from "./models/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { User, Expense } = models;
 
 // Middleware
 app.use(cors({ origin: /^http:\/\/localhost:\d+$/, credentials: true }));
@@ -14,12 +15,28 @@ app.get("/", (req, res) => {
   res.json({ message: "Server is running!" });
 });
 
+app.post("/new-expense", async (req, res) =>{
+  const newExpense = Expense.create(req.body)
+  console.log(newExpense.toJSON());
+  res.status(200).json({ message: "Expense created successfully!" });
+});
+
+app.put("/update-expense/", async (req, res) => {
+  const user = req.body.user;
+  if(!user.role || user.role !== "Director" || !user.role == "Exec") {
+    return res.status(403).json({ message: "Forbidden: Only admins can update expenses." });
+  }
+
+  const expenseId = req.body.expenseId;
+  
+})
+
 // Sync DB and start server
 sequelize
   .authenticate()
   .then(() => {
     console.log("Database connected.");
-    return sequelize.sync({ alter: true }); // or { force: true } to reset tables
+    return sequelize.sync({ alter: true });
   })
   .then(() => {
     app.listen(PORT, () => {
